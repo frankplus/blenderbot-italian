a9aaefa200f87704796dd4cc623b022234dc6672
Francesco <pham.francesco@gmail.com>
2021-03-22 18:59:31 +0100

    update drive link

--- download_model.py
+++ download_model.py
@@ -1,6 +1,6 @@
 import gdown
 
-url = 'https://drive.google.com/uc?id=1Z4aWbKAEulcg-ykGrd-ugnx1IUDajepn'
+url = 'https://drive.google.com/uc?id=1Hih1QAZSM-W07UVSmhFfZhEWYcDNzjfJ'
 output = 'blenderbot_italian.zip'
 
 gdown.cached_download(url, output, postprocess=gdown.extractall)
